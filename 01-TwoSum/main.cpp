9b68af3058fea4ef25fea92c90b5568d55d6d87a
dennisliuu <dennisliuu@gmail.com>
2019-08-21 21:52:50 +0800

    update 01

--- 01-TwoSum/main.cpp
+++ 01-TwoSum/main.cpp
@@ -7,13 +7,13 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-		unordered_map<int, int> map;
+		unordered_map<int, int> un_map;
 		for (int i = 0; i < nums.size(); i++)
-			map[nums[i]] = i;
+			un_map[nums[i]] = i;
 		for (int i = 0, res = 0; i < nums.size(); i++) {
 			res = target - nums[i];
-			if(map.count(res) && map[res] != i)
-				return {i, map[res]};
+			if(un_map.count(res) && un_map[res] != i)
+				return {i, un_map[res]};
 		}
 		return {};
     }
@@ -26,8 +26,8 @@ int main(void) {
     vect.push_back(2);
 	vect.push_back(4);
 
-	vector<int> path = s.twoSum(vect, 6);
-	for (auto i = path.begin(); i != path.end(); ++i)
+	vector<int> ans = s.twoSum(vect, 6);
+	for (auto i = ans.begin(); i != ans.end(); ++i)
     	std::cout << *i << ' ';
 	return 0;
 }
