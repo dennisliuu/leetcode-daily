a608411e06dabe068c1990503c7846d38f968752
dennisliuu <dennisliuu@gmail.com>
2019-08-24 19:20:48 +0800

    update code and readme

--- 237-Delete-Node-in-a-Linked-List/README.md
+++ 237-Delete-Node-in-a-Linked-List/README.md
@@ -6,8 +6,28 @@ Time complexity: O(n)
 
 ### 想法：
 
+題目給定一個 Singly Linked List，並給予該 Linked list 的 head。
 
+題目透過傳遞一個 val，並要求從該 Linked list 中刪除該點。
+
+故可以透過將該點的指標 ptr 轉至下一個 val，並完成刪除
 
 ### 技術筆記
 
-Linked-list
+Struct: 結構，自訂不同資料型態串在一起
+
+```
+Singly Linklist 結構為一指標（指下一個）與該點之值
+
+宣告：
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+使用方式：
+struct ListNode* ptr;
+
+```
--- 237-Delete-Node-in-a-Linked-List/main.cpp
+++ 237-Delete-Node-in-a-Linked-List/main.cpp
@@ -21,19 +21,47 @@ public:
     }
 };
 
+struct ListNode *head = NULL;
+void insert(int new_data)
+{
+    struct ListNode *new_node = (struct ListNode *)malloc(sizeof(struct ListNode));
+    new_node->val = new_data;
+    new_node->next = head;
+    head = new_node;
+}
+
+void display() { 
+   struct ListNode* ptr;
+   ptr = head;
+   while (ptr != NULL) { 
+      cout<< ptr->val <<" "; 
+      ptr = ptr->next; 
+   } 
+} 
+
 int main(void)
 {
     Solution s;
-    vector<int> vect;
-    vect.push_back(4);
-    vect.push_back(5);
-    vect.push_back(1);
-    vect.push_back(9);
-    
-    // vector<int> ans = s.deleteNode(ListNode(5));
 
-    // for (auto i = ans.begin(); i != ans.end(); ++i)
-    //     std::cout << *i << ' ';
+    insert(4);
+    insert(1);
+    insert(5);
+    insert(3);
+    
+    struct ListNode* ptr;
+    ptr = head;
+
+    while (ptr->next != NULL)
+    {
+        if (ptr->val == 5)
+        {
+            s.deleteNode(ptr);
+        }
+        else
+            ptr = ptr->next;
+    }
+
+    display();
 
     return 0;
 }
