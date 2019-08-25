55394b944dab82f0ca920c8e6e411977736dbd88
dennisliuu <dennisliuu@gmail.com>
2019-08-25 19:48:32 +0800

    add 1108

---
+++ 1108-Defanging-an-IP-Address/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        std::string output;
+        output.reserve(address.size());
+        for (const char c: address) {
+            switch (c) {
+                case '.':  output += "[.]";        break;
+                default:    output += c;            break;
+            }
+        }
+        return output;
+    }
+};
+
+int main (void) {
+    Solution s;
+    cout << s.defangIPaddr("1.1.1.1") << endl;
+
+    return 0;
+}
