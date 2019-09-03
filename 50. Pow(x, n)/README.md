f24cb368dbff690eee5cda9c467d36e0e3db0a01
dennisliuu <dennisliuu@gmail.com>
2019-09-03 21:11:41 +0800

    add 50

---
+++ 50. Pow(x, n)/README.md
@@ -0,0 +1,11 @@
+```
+Environment: gcc version 7.4.0
+Date: 2019//
+Time complexity: O(n)
+```
+
+### 想法：
+
+內建狗
+
+### 技術筆記
---
+++ 50. Pow(x, n)/main.cpp
@@ -0,0 +1,8 @@
+#include <math.h>
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x, n);
+    }
+};
