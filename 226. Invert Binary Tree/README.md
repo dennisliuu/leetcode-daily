e541b9bf56a918205f309a737c94f1cd10163487
dennisliuu <dennisliuu@gmail.com>
2019-09-16 00:24:19 +0800

    add 226

---
+++ 226. Invert Binary Tree/README.md
@@ -0,0 +1,13 @@
+```
+Environment: gcc version 7.4.0
+Date: 2019/9/16
+Time complexity: O(n)
+```
+
+### 想法：
+
+直接 swap(root->left, root->right)，直觀作法，
+
+swap 完後就在到子樹去遞歸
+
+### 技術筆記
---
+++ 226. Invert Binary Tree/main.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (root == nullptr)
+            return nullptr;
+        std::swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
